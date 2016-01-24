4506287f16850677e92916b5d726d721955c9d9a
meintte <daniel_stalder@hispeed.ch>
2016-01-24 13:40:16 +0100

    Main Class, first commit

---
+++ Maturaarbeit/src/Main.java
@@ -0,0 +1,243 @@
+import com.jogamp.common.nio.Buffers;
+import com.jogamp.opengl.GL;
+import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLEventListener;
+import com.jogamp.opengl.GLProfile;
+import com.jogamp.opengl.GLCapabilities;
+import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.fixedfunc.GLMatrixFunc;
+import com.jogamp.opengl.fixedfunc.GLPointerFunc;
+import com.jogamp.opengl.glu.GLU;
+
+import javax.swing.JFrame;
+
+import java.awt.BorderLayout;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+
+import com.jogamp.opengl.util.FPSAnimator;
+
+
+public class Main extends JFrame
+    implements GLEventListener, KeyListener, MouseListener
+{
+ 
+	private GLU glu;
+	private GLCapabilities caps;
+	private static GLCanvas canvas; 
+	private camera camera = new camera(0,-2,-10);
+	
+	private Boolean drawBottom = true;
+
+	//constructor
+	public Main()
+	{
+		super("Main");
+		//
+		caps = new GLCapabilities(GLProfile.getDefault());
+		canvas = new GLCanvas(caps);
+		canvas.addGLEventListener(this);
+		canvas.addKeyListener(this);
+		canvas.addMouseListener(this);
+		//
+		getContentPane().add(canvas, BorderLayout.CENTER);
+	} // end of constructor
+ 
+	public void run()
+	{
+		setSize(640, 480);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		canvas.requestFocusInWindow();
+		
+	} // end of run
+	
+	public static void main(String[] args)
+	{
+		new Main().run();
+		final FPSAnimator animator = new FPSAnimator(canvas, 60,true); 
+	    animator.start(); 
+	} // end of main
+ 
+// ------------------ OpenGL Part  ------------------ //
+  
+	public void init(GLAutoDrawable drawable)
+	{
+		GL2 gl = drawable.getGL().getGL2();  
+		glu = new GLU();
+		gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+		// enables depth_test (which vertex is behind the other...)
+		gl.glEnable(GL.GL_DEPTH_TEST);
+		
+		setupPointers(gl);
+	} // end of init
+ 
+	public void display(GLAutoDrawable drawable)
+	{
+		GL2 gl = drawable.getGL().getGL2();
+		gl.glClear( GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+		
+		//camera
+		gl.glMatrixMode( GLMatrixFunc.GL_PROJECTION );
+        gl.glLoadIdentity();
+		glu.gluPerspective(camera.fovy, camera.aspect, camera.near, camera.far);
+        gl.glTranslated(camera.x, camera.y, camera.z);
+        
+        //Modeling
+		gl.glMatrixMode(GLMatrixFunc.GL_MODELVIEW);
+		gl.glLoadIdentity();
+		
+		if(drawBottom){
+			gl.glBegin(GL.GL_TRIANGLE_STRIP);
+			gl.glColor3d(.3, .3, .3);
+			gl.glVertex3d(100,0,-100);
+			gl.glVertex3d(-100, 0, -100);
+			gl.glVertex3d(100, 0, 100);
+			gl.glVertex3d(-100, 0, 100);
+			gl.glEnd();
+		}
+		int indices[] = new int[] { 0, 2, 1,   1, 3, 2,   3, 4, 2,   0, 4, 2 };
+		IntBuffer indicesBuf = Buffers.newDirectIntBuffer(indices.length);
+			
+		for (int i = 0; i < indices.length; i++)
+			indicesBuf.put(indices[i]);
+			indicesBuf.rewind();
+			gl.glDrawElements(GL2.GL_TRIANGLES, indices.length, GL.GL_UNSIGNED_INT, indicesBuf);
+		
+		gl.glFlush();
+	}
+
+	@Override
+	public void reshape(GLAutoDrawable drawable, int x, int y, int w, int h)
+	{
+		GL2 gl = drawable.getGL().getGL2();
+
+		camera.aspect = (double)w/h;
+		gl.glViewport(0, 0, w, h);
+  
+	} //end of reshape
+  
+	@Override
+	public void dispose(GLAutoDrawable drawable) {
+  	
+	}
+  
+//------------------ Pointers Part  ------------------ //
+ 
+	private void setupPointers(GL2 gl)
+	{
+		float vertices[] = new float[]	
+				{  		3, -1, 3,   	-3, 0, 3,   	0, 1.5f, 0, 
+						-3, 2.5f, -3, 	 3, 1.5f, -3,
+				};
+		float colors[] = new float[]
+				{  		1, 0, 0, 	1, 2, 0, 	0, 1, 0, 
+						1, 0, 0, 	1, 0, 0, 	
+				};
+		/*int normals[] = new int[]
+				{ 		1, 0, 0, 	1, 0, 0, 	1, 0, 0, 
+						1, 0, 0, 	1, 0, 0, 	1, 0, 0,
+						1, 0, 0, 	1, 0, 0, 	1, 0, 0,
+						1, 0, 0, 	1, 0, 0, 	1, 0, 0,
+				};*/
+		FloatBuffer tmpVerticesBuf = Buffers.newDirectFloatBuffer(vertices.length);
+		FloatBuffer tmpColorsBuf = Buffers.newDirectFloatBuffer(colors.length);
+		//IntBuffer tmpNormalsBuf = Buffers.newDirectIntBuffer(normals.length);
+		for (int i = 0; i < vertices.length; i++)
+			tmpVerticesBuf.put(vertices[i]);
+		for (int j = 0; j < colors.length; j++)
+			tmpColorsBuf.put(colors[j]);
+		/*for (int k = 0; k < normals.length; k++)
+			tmpNormalsBuf.put(normals[k]);*/
+		tmpVerticesBuf.rewind();
+		tmpColorsBuf.rewind();
+		//tmpNormalsBuf.rewind();
+		
+		//
+		gl.glEnableClientState(GLPointerFunc.GL_VERTEX_ARRAY);
+		gl.glEnableClientState(GLPointerFunc.GL_COLOR_ARRAY);
+		//gl.glEnableClientState(GLPointerFunc.GL_NORMAL_ARRAY);
+		//
+		gl.glVertexPointer(3, GL.GL_FLOAT, 0, tmpVerticesBuf);
+		gl.glColorPointer(3, GL.GL_FLOAT, 0, tmpColorsBuf);
+		//gl.glNormalPointer(GL2ES2.GL_INT, 0, tmpNormalsBuf);
+ 
+	} // end of setupPointers
+  
+//------------------ Event Part  ------------------ //
+  
+	public void keyTyped(KeyEvent key)
+	{ 
+	
+	}
+ 
+	public void keyPressed(KeyEvent key)
+	{
+		switch (key.getKeyChar()) 
+		{
+		case 'w':
+			camera.translate(0, 0, .1);
+			break;
+		case 's':
+			camera.translate(0, 0, -.1);
+			break;
+		case 'a':
+			camera.translate(.1, 0, 0);
+			break;
+		case 'd':
+			camera.translate(-.1, 0, 0);
+			break;
+		case 'e':
+			camera.translate(0, .1, 0);
+			break;
+		case 'q':
+			camera.translate(0, -.1, 0);
+			break;
+		case KeyEvent.VK_SPACE:
+			System.out.println(camera.x + " " + camera.y + " " + camera.z);
+			break;
+		case KeyEvent.VK_ESCAPE:
+			System.exit(0);
+			break;
+ 
+		default:
+			break;
+		} // end of switch
+	} // end of keyPressed
+ 
+	public void keyReleased(KeyEvent key)
+	{ 
+  
+	}
+ 
+	public void mouseClicked(MouseEvent mouse)
+	{  
+  
+	}
+ 
+	public void mousePressed(MouseEvent mouse)
+	{
+	
+	}
+ 
+	public void mouseReleased(MouseEvent mouse)
+	{
+  
+	}
+	
+	public void mouseEntered(MouseEvent mouse)
+	{
+
+	}
+ 
+	public void mouseExited(MouseEvent mouse)
+	{
+
+	}
+}
